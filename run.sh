7626d22b2e40b5079a6d887dfced00d0323b6b68
Elvis Attro <attroelvis@gmail.com>
2025-08-11 13:37:33 +0000

    update run.sh file.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #!/bin/sh -l
 set -e
 
-git config --global user.email $GITHUB_USER_EMAIL
-git config --global user.name $GITHUB_USERNAME
-git clone https://$4@github.com/$5.git  $RUNNER_TEMP/infra-as-code-repo
+git config --global user.email "$GITHUB_USER_EMAIL"
+git config --global user.name "$GITHUB_USERNAME"
+
+git clone "https://$4@github.com/$5.git"  "$RUNNER_TEMP/infra-as-code-repo"
 wget https://raw.githubusercontent.com/DenisPalnitsky/gitops-release/master/replace-key.py
-python replace-key.py --file $RUNNER_TEMP/infra-as-code-repo/$1 --key $2 --value $3
-cd $RUNNER_TEMP/infra-as-code-repo
+python replace-key.py --file $RUNNER_TEMP/infra-as-code-repo/"$1" --key $2 --value $3
+cd "$RUNNER_TEMP/infra-as-code-repo"
+if [ -n "$6" ]; then
+  git checkout "$6" 2>/dev/null || git checkout -b "$6"
+fi
 git add .
-git commit -m"Release of key $2 in $1"
-git push
+git commit -m "Updating of key $2 in $1"
+git push -u origin "$6"
