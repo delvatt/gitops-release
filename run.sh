f0dc873072276711f55b8849444402957cda63b8
Elvis Attro <attroelvis@gmail.com>
2025-08-11 16:59:28 +0000

    fix some few bugs.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- run.sh
+++ run.sh
@@ -5,17 +5,18 @@ git config --global user.email "$GITHUB_USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
 
 git clone "https://$4@github.com/$5.git"  "$RUNNER_TEMP/infra-as-code-repo"
+cd "$RUNNER_TEMP/infra-as-code-repo"
 
 git --no-pager branch -a -v
 
 if [ -n "$6" ]; then
-  git checkout -b "$6" remotes/origin/$6
+  git checkout -b "$6" "origin/$6"
 fi
 
 wget https://raw.githubusercontent.com/delvatt/gitops-release/master/replace-key.py
-python replace-key.py --file $RUNNER_TEMP/infra-as-code-repo/"$1" --key $2 --value $3
+python replace-key.py --file "$1" --key "$2" --value "$3"
+rm replace-key.py
 
-cd "$RUNNER_TEMP/infra-as-code-repo"
 if [ -n "$7" ]; then
   git checkout "$7" 2>/dev/null || git checkout -b "$7"
 fi
