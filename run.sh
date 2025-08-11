e09cc1468d6cba1ea883f7567f7d939418138f3b
Elvis Attro <attroelvis@gmail.com>
2025-08-11 16:34:30 +0000

    add debugging statements.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- run.sh
+++ run.sh
@@ -5,15 +5,21 @@ git config --global user.email "$GITHUB_USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
 
 git clone "https://$4@github.com/$5.git"  "$RUNNER_TEMP/infra-as-code-repo"
+
+git --no-pager branch -a -v
+
 if [ -n "$6" ]; then
-  git checkout -b "$6" "origin/$6"
+  git checkout -b "$6" remotes/origin/$6
 fi
+
 wget https://raw.githubusercontent.com/delvatt/gitops-release/master/replace-key.py
 python replace-key.py --file $RUNNER_TEMP/infra-as-code-repo/"$1" --key $2 --value $3
+
 cd "$RUNNER_TEMP/infra-as-code-repo"
 if [ -n "$7" ]; then
   git checkout "$7" 2>/dev/null || git checkout -b "$7"
 fi
+
 git add .
 git commit -m "Updating of key $2 in $1"
 git push -u origin "$7"
